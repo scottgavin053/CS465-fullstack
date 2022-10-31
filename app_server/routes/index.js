78a1eab321827a0d19447c413a5c0b5097094c56
Gavin Scott <scottgavin053@gmail.com>
2022-10-30 19:44:46 -0500

    Adding MVC functionality

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', ctrlMain);
+router.get('/', ctrlMain.index);
 
 module.exports = router;
