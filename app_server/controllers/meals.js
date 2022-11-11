3048f0826d0428fba15377252f0cf512400d285a
Gavin Scott <scottgavin053@gmail.com>
2022-11-11 00:30:01 -0600

    Converted static html to JSON for rooms and meals pages

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,8 +1,11 @@
+var fs = require('fs');
+var mealList = JSON.parse(fs.readFileSync('./data/meals.json', 'utf8'));
+
 /* GET meals view */
 const meals = (req, res) => {
      
     pageTitle =  'Travlr Getaways - Meals';
-    res.render('meals', { title: pageTitle });
+    res.render('meals', { title: pageTitle, mealList });
     };
 
     module.exports = {
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,8 +1,11 @@
+var fs = require('fs');
+var roomList = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+
 /* GET rooms view */
 const rooms = (req, res) => {
      
     pageTitle =  'Travlr Getaways - Rooms';
-    res.render('rooms', { title: pageTitle });
+    res.render('rooms', { title: pageTitle, roomList });
    };
 
    module.exports = {
