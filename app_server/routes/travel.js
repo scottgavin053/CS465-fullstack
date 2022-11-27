41ff22b8e7c2d05fcba8d0e99ef67d638e0837bf
Gavin Scott <scottgavin053@gmail.com>
2022-11-26 23:52:34 -0600

    Use API endpoints

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var controller = require('../controllers/travel');
 
 /* GET home page. */
-router.get('/', controller.travel);
+router.get('/', controller.travelList);
 
 module.exports = router;
